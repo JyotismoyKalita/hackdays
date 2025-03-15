fa2ececec219499b5969816953f5d84ab060a671
GourabAdhikari <gourabadhikari24@gmail.com>
2025-03-15 21:42:09 +0530

    add herosection

---
+++ src/components/Featured1.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Featured1() {
+  return (
+     <div className="py-12 bg-black ">
+        
+            <div className="text-center mt-10">
+                <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">Our Key Features</h2>
+                <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Discover features that make a difference !</p>
+            </div>
+            </div>
+  )
+}
+
+export default Featured1
---
+++ src/components/FeaturedSec.tsx
@@ -0,0 +1,69 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
+import {
+  IconArrowWaveRightUp,
+  IconBoxAlignRightFilled,
+  IconBoxAlignTopLeft,
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
+
+export function BentoGridDemo() {
+  return (
+    <BentoGrid className="max-w-4xl mt-10">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          icon={item.icon}
+          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+        />
+      ))}
+    </BentoGrid>
+  );
+}
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+const items = [
+  {
+    title: "AI Recomended Stock Level",
+    description: "The  stockthat should be kept by the Owner is recommended by AI to preventexcess stock and wastage.",
+    header: <img  src="http://localhost:3000/stock copy.jpg" alt="" className="w-65 h-37" />,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "AI Recomended Products ",
+    description: "Based on trends, AI wouldrecommend new products not currently in the stock to improveprofit of the business.",
+    header: <img  src="http://localhost:3000/products copy.jpg" alt="" className="w-65 h-37" />,
+    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "AI recomended Dynamic Pricing",
+    description: "Discover the beauty of thoughtful and functional design.",
+    header: <img  src="http://localhost:3000/pexels-pixabay-259027 copy.jpg" alt="" className="w-65 h-37" />,
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "AI Recomended discontinuation of a particular item",
+    description:
+      "Based on demands and sale, AI would recommend what items to not reorder for next batch of stock.",
+    header:  <img  src="http://localhost:3000/need2 copy.jpg" alt="" className="w-full h-45" />,
+    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  },
+  
+  {
+    title: "Expiry Tracker",
+    description: " Expiry dates of products which can expire is tracked and when they are about to expire, the owner is alerted,making it easier for the owner to know about the currently expiring products.",
+    header: <img  src="http://localhost:3000/warning copy.jpg" alt="" className="w-65 h-27" />,
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+  
+ 
+];
+export default BentoGridDemo;
