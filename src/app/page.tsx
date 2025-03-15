871cd3bb8c131e6dd5cbbcda22e33c969f724db1
GourabAdhikari <gourabadhikari24@gmail.com>
2025-03-15 22:00:04 +0530

    fix ESlint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
-
 import HeroSection from '../components/HeroSection';
 import Featured1 from '@/components/Featured1';
 import FeaturedSec from '@/components/FeaturedSec';
 
 export default function Page() {
-  return (
-   <main className="=min-h-screen bg-black antialiased bg-grid-white/[0.02]">
-     
-     <HeroSection />
-     <Featured1 />
-     <FeaturedSec />
-     
-     
-     
-      
-      
-   </main>
-  );
+    return (
+        <main className="=min-h-screen bg-black antialiased bg-grid-white/[0.02]">
+            <HeroSection />
+            <Featured1 />
+            <FeaturedSec />
+        </main>
+    );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,38 +1,35 @@
-"use client";
-import React from "react";
-import { Spotlight } from "@/components/ui/spotlight-new";
-import { TextGenerateEffect } from "./ui/text-generate-effect";
-import Link from "next/link";
-import { Button } from "@/components/ui/moving-border";
+'use client';
+import React from 'react';
+import { Spotlight } from '@/components/ui/spotlight-new';
+import { TextGenerateEffect } from './ui/text-generate-effect';
+import Link from 'next/link';
+import { Button } from '@/components/ui/moving-border';
 
 const words = `Smarter Inventory,Seamless Operations`;
 
-
 export function SpotlightNewDemo() {
-  return (
-    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 transform translate-y-30">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-       InvenTrack <br /> 
-        </h1>
-        <div className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-        <TextGenerateEffect words={words} />
-       
-          
+    return (
+        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+            <Spotlight />
+            <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 transform translate-y-30">
+                <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+                    InvenTrack <br />
+                </h1>
+                <div className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+                    <TextGenerateEffect words={words} />
+                </div>
+                <div className="mt-8 flex justify-center transform translate-y-10">
+                    <Link href="/courses">
+                        <Button
+                            borderRadius="1.75rem"
+                            className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                        >
+                            Getting Started
+                        </Button>
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className="mt-8 flex justify-center transform translate-y-10">
-        <Link href="/courses">
-                <Button 
-                borderRadius='1.75rem'
-                className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800">
-                Getting Started
-                </Button>
-                </Link>
-        </div>
-               
-          
-      </div>
-    </div>
-  );
-}export default SpotlightNewDemo;
+    );
+}
+export default SpotlightNewDemo;
