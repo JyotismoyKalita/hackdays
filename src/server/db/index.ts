759167d738674f26f4b9bb66b5ad7d02bdad2fcc
Arnab Chakraborty <iam-arnab@outlook.com>
2025-03-15 23:02:03 +0530

    refactor: remove dotenv dependency and update database schema with new tables

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,6 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
-import { config } from 'dotenv';
-
-config({ path: '.env' });
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,42 @@
-import { pgTable, text, varchar } from 'drizzle-orm/pg-core';
+import {
+    boolean,
+    date,
+    integer,
+    pgTable,
+    text,
+    uuid,
+    varchar,
+} from 'drizzle-orm/pg-core';
 
 export const UserTable = pgTable('user', {
     id: text('id').primaryKey().notNull(),
     name: varchar('name', { length: 255 }).notNull(),
+});
+
+export const CompanyTable = pgTable('company', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    name: varchar('name', { length: 255 }).notNull(),
+    userId: text('userId')
+        .references(() => UserTable.id)
+        .notNull(),
+});
+
+export const ItemTable = pgTable('item', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    model: varchar('model', { length: 255 }).notNull(),
+    expired: boolean('expired').notNull(),
+    expiryDate: date('expiryDate').notNull(),
+    quantity: integer('quantity').notNull(),
+    companyId: uuid('companyId')
+        .references(() => CompanyTable.id)
+        .notNull(),
+});
+
+export const SoldItemTable = pgTable('soldItem', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    model: varchar('model', { length: 255 }).notNull(),
+    quantity: integer('quantity').notNull(),
+    companyId: uuid('companyId')
+        .references(() => CompanyTable.id)
+        .notNull(),
 });
