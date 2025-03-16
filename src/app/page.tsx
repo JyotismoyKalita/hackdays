6f025b20cd88b0e5ce95600f4a3374b5bd3d161c
GourabAdhikari <gourabadhikari24@gmail.com>
2025-03-16 14:42:34 +0530

    add globe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,13 @@ import Featured1 from '@/components/Featured1';
 import FeaturedSec from '@/components/FeaturedSec';
 import Instructors from '@/components/Instructors';
 import Footer from '@/components/Footer';
-<<<<<<< HEAD
 import Faq from '@/components/Faq';
 import { Globe } from '@/components/ui/globe';
-=======
-import FaqSectionWithCategoriesDisplay from '@/components/Faq';
->>>>>>> 5da373f34d609f16b17969c5ff6a60b59e937554
 
 export default function Page() {
     return (
         <main className="min-h-screen bg-black antialiased bg-grid-white/[0.02]">
             <HeroSection />
-<<<<<<< HEAD
             <div className="space-y-16">
                 <Featured1 />
                 <FeaturedSec />
@@ -27,12 +22,11 @@ export default function Page() {
                 <Globe />
             </div>
             <Faq />
-=======
             <Featured1 />
             <FeaturedSec />
+            <FeaturedSec />
             <Instructors />
-            <FaqSectionWithCategoriesDisplay />
->>>>>>> 5da373f34d609f16b17969c5ff6a60b59e937554
+            <Faq />
             <Footer />
         </main>
     );
