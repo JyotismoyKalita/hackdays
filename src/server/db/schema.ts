b0b9ffb184fb7a97e9d067102ebe49a1d3dc6793
Arnab Chakraborty <iam-arnab@outlook.com>
2025-03-17 22:14:27 +0530

    feat: add relations for Company, User, Item, and SoldItem tables in schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from 'drizzle-orm';
 import {
     boolean,
     date,
@@ -56,4 +57,42 @@ export const SoldItemTable = pgTable('soldItem', {
     companyId: uuid('companyId')
         .references(() => CompanyTable.id)
         .notNull(),
+});
+
+export const CompanyTableRelations = relations(
+    CompanyTable,
+    ({ one, many }) => {
+        return {
+            user: one(UserTable, {
+                fields: [CompanyTable.userId],
+                references: [UserTable.id],
+            }),
+            items: many(ItemTable),
+            soldItems: many(SoldItemTable),
+        };
+    }
+);
+
+export const UserTableRelations = relations(UserTable, ({ many }) => {
+    return {
+        companies: many(CompanyTable),
+    };
+});
+
+export const ItemTableRelations = relations(ItemTable, ({ one }) => {
+    return {
+        company: one(CompanyTable, {
+            fields: [ItemTable.companyId],
+            references: [CompanyTable.id],
+        }),
+    };
+});
+
+export const SoldItemTableRelations = relations(SoldItemTable, ({ one }) => {
+    return {
+        company: one(CompanyTable, {
+            fields: [SoldItemTable.companyId],
+            references: [CompanyTable.id],
+        }),
+    };
 });
