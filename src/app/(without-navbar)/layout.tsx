075381db4e324be94f2785f2b6d24bce68a68cda
Arnab Chakraborty <iam-arnab@outlook.com>
2025-03-18 13:49:50 +0530

    move sidebar to layout

--- src/app/(without-navbar)/layout.tsx
+++ src/app/(without-navbar)/layout.tsx
@@ -1,8 +1,23 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '../globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider, UserButton } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -39,7 +54,37 @@ export default function RootLayout({
                     className={`${geistSans.variable} ${geistMono.variable} antialiased`}
                 >
                     <div className="relative w-full flex items-center justify-center"></div>
-                    {children}
+                    <SidebarProvider>
+                        <AppSidebar />
+                        <SidebarInset>
+                            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                                <SidebarTrigger className="-ml-1" />
+                                <Separator
+                                    orientation="vertical"
+                                    className="mr-2 data-[orientation=vertical]:h-4"
+                                />
+                                <Breadcrumb>
+                                    <BreadcrumbList>
+                                        <BreadcrumbItem className="hidden md:block">
+                                            <BreadcrumbLink href="#">
+                                                InvenTrack
+                                            </BreadcrumbLink>
+                                        </BreadcrumbItem>
+                                        <BreadcrumbSeparator className="hidden md:block" />
+                                        <BreadcrumbItem>
+                                            <BreadcrumbPage>
+                                                Overview
+                                            </BreadcrumbPage>
+                                        </BreadcrumbItem>
+                                    </BreadcrumbList>
+                                </Breadcrumb>
+                                <div className="ml-auto">
+                                    <UserButton />
+                                </div>
+                            </header>
+                            {children}
+                        </SidebarInset>
+                    </SidebarProvider>
                 </body>
             </html>
         </ClerkProvider>
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { GalleryVerticalEnd } from 'lucide-react';
 import { SearchForm } from '@/components/search-form';
 
 import {
@@ -81,9 +80,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                     <SidebarMenu>
                         <SidebarMenuItem>
                             <SidebarMenuButton size="lg">
-                                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                                    <GalleryVerticalEnd className="size-4" />
-                                </div>
                                 <div className="flex flex-col gap-0.5 leading-none">
                                     <span className="font-medium">
                                         Dashboard
