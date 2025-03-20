71abebe562e696a777f6421277a868bb02777a71
GourabAdhikari <gourabadhikari24@gmail.com>
2025-03-20 11:07:55 +0530

    add newmodifications

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,7 +9,7 @@ const words = `Smarter Inventory,Seamless Operations`;
 
 export function SpotlightNew() {
     return (
-        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden dark">
+        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden dark min-h-180">
             <Spotlight />
             <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 transform translate-y-30">
                 <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
