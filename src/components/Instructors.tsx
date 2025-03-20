5870644ecfc3e1defa3df428d50c126f4f390fbd
GourabAdhikari <gourabadhikari24@gmail.com>
2025-03-20 10:56:15 +0530

    add newmodification

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -40,7 +40,8 @@ function Instructors() {
         <div className="relative h-[40rem] overflow-hidden flex items-center justify-center">
             <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full">
                 <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">
-                    Meet Our Key Developers
+                    Meet Our Key{' '}
+                    <span className="text-teal-600">Developers</span>
                 </h2>
                 <p className="text-base md:text-lg text-white text-center mb-4">
                     Shaping ideas into reality with code and creativity
